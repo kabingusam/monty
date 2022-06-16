09ee4d81d839a8b01346c7bfab1bb2625a1da933
Kabingu Sammy <80510478+kabingusam@users.noreply.github.com>
2022-06-16 15:47:56 +0300

    update

--- print_function4.c
+++ print_function4.c
@@ -62,6 +62,7 @@ void monty_rotr(stack_t **stack, unsigned int line_number)
  * @stack: A pointer to the top mode node of a stack_t linked list.
  * @line_number: The current working line number of a Monty bytecodes file.
  */
+ 
 void monty_stack(stack_t **stack, unsigned int line_number)
 {
 	(*stack)->n = STACK;
